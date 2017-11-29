eace07bfe85beaeae9bdfb9166713c1e55f93c4d
[bandolerolee@gmail.com] <[bandolerolee@gmail.com]>
2017-11-29 17:38:32 +0800

    update 5.0.0

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
-import { AngularPluginPage } from './app.po';
+import { AppPage } from './app.po';
 
-describe('angular-plugin App', function() {
-  let page: AngularPluginPage;
+describe('a App', () => {
+  let page: AppPage;
 
   beforeEach(() => {
-    page = new AngularPluginPage();
+    page = new AppPage();
   });
 
-  it('should display message saying app works', () => {
+  it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
   });
 });
